d42b77a0727d3cffd1a27b64abcd448fa47e9d43
Fred Sheppard <stewart.sheppard@my.tccd.edu>
2020-04-10 03:29:35 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,8 @@
+//Student Name: Stewart Sheppard
+//Teacher: Dr. Tyson McMillan
+//Date: 04-09-20
+//A program to practice working with multi-dimensional arrays
+
+#include <iostream>
+using namespace std;
+
